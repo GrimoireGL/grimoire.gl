50add61ce63456d8c78ed1f057aa6859be63450b
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-08-01 01:12:22 +0900

    fix: Fixed layout of shader sample

--- themes/grimoire/source/gr/shading/main.js
+++ themes/grimoire/source/gr/shading/main.js
@@ -1,7 +1,9 @@
 gr(function(){
   hue = 0;
   document.querySelector("#shading").addEventListener("mousemove",()=>{
-    hue+=0.003;
-    gr("#shading .canvas")("mesh").setAttribute("hue",hue%1);
+    hue=(hue + 0.003) % 1;
+    gr("#shading .canvas")("mesh").setAttribute("hue",hue);
+    document.querySelector(".hue").textContent = Math.floor(hue * 360) +"°";
+    document.querySelector(".hue").style.color = "hsl(" + hue * 360 +",50%,50%)"
   });
 });
