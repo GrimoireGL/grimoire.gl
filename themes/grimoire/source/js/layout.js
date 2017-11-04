0fd5c98bebb96d7e483b52f0f1c95bce6b795198
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-11-05 00:14:30 +0900

    fix: Support mobile browser

--- themes/grimoire/source/js/layout.js
+++ themes/grimoire/source/js/layout.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // activate humburger button
   var el = document.querySelectorAll('.hamburger');
   var sidebar = document.querySelector('#sidebar');
   var menu = document.querySelector('#menu-wrap');
   for (var i = 0; i < el.length; i++) {
-    el[i].addEventListener('click', function() {
+    el[i].addEventListener('click', function () {
       this.classList.toggle('active');
       sidebar.classList.toggle('active');
       menu.classList.toggle('active');
@@ -21,34 +21,34 @@ document.addEventListener('DOMContentLoaded', function() {
   //
   var marginComputedStyle = window.getComputedStyle(document.querySelector('#content'), '');
   var marginTop = parseFloat(marginComputedStyle.marginTop.slice(0, -2)) + parseFloat(marginComputedStyle.paddingTop.slice(0, -2));
-  var hArr = Array.from(document.querySelectorAll('h1, h2')).filter(function(v) {
+  var hArr = Array.from(document.querySelectorAll('h1, h2')).filter(function (v) {
     return v.id !== 'content-title';
   });
-  var subMenu = hArr.map(function(v) {
+  var subMenu = hArr.map(function (v) {
     return createSubMenu(v, marginTop);
   });
 
   // add submenu in sidebar
   var elm = document.createElement('ul');
   elm.setAttribute('class', 'sub-menu');
-  subMenu.forEach(function(v) {
+  subMenu.forEach(function (v) {
     elm.appendChild(v);
   });
   document.querySelector('#list .active').appendChild(elm);
 
   // update submenu
   updateSubMenuActive(hArr, subMenu, marginTop);
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', function () {
     updateSubMenuActive(hArr, subMenu, marginTop);
   });
-  window.addEventListener('resize', function() {
+  window.addEventListener('resize', function () {
     updateSubMenuActive(hArr, subMenu, marginTop);
   });
 
   // override title anchor link
-  Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6')).forEach(function(v) {
+  Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6')).forEach(function (v) {
     if (v.id !== 'content-title') {
-      v.querySelector('a').addEventListener('click', function(e) {
+      v.querySelector('a').addEventListener('click', function (e) {
         e.preventDefault();
         window.scroll(0, v.offsetTop - marginTop);
       });
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var href = document.location.href;
   if (hash !== '') {
     document.location.hash = '';
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
       window.scroll(0, document.querySelector(hash).offsetTop - marginTop);
       history.replaceState(null, null, href);
     });
@@ -72,7 +72,7 @@ function createSubMenu(h, marginTop) {
   var a = document.createElement('a');
   a.innerText = h.innerText;
   a.setAttribute('href', '#' + h.id);
-  a.addEventListener('click', function(e) {
+  a.addEventListener('click', function (e) {
     e.preventDefault();
     document.location.hash = h.id;
     window.scroll(0, h.offsetTop - marginTop);
--- themes/grimoire/source/js/load-contributors.js
+++ themes/grimoire/source/js/load-contributors.js
@@ -1,39 +1,39 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET","/contributors.json");
+xhr.open("GET", "/contributors.json");
 xhr.send();
-xhr.addEventListener('load',function(){
-    var response = JSON.parse(xhr.responseText);
-    for(var type in response){
-      var typeContent = response[type];
-      var typeContainer =  document.createElement('div');
-      var typeHeader = document.createElement('h3');
-      typeHeader.innerText = type;
-      typeContainer.className = "contributors-type-container";
-      var avatarsContainer = document.createElement('div');
-      avatarsContainer.className = 'contributors-avatar-container';
-      typeContainer.appendChild(typeHeader);
-      typeContainer.appendChild(avatarsContainer);
-      for(var name in typeContent){
-        var userContent = typeContent[name];
-        var avatar = document.createElement('div');
-        var img = document.createElement('img');
-        var sn = document.createElement('a');
-        avatar.setAttribute('class', 'wrap');
-        img.setAttribute('src', userContent.img);
-        sn.innerText = name;
-        sn.setAttribute('class', 'screen-name');
-        sn.setAttribute('href', "https://github.com/" + name);
-        sn.setAttribute('target', '_blank');
-        avatar.appendChild(img);
-        avatar.appendChild(sn);
-        avatarsContainer.appendChild(avatar);
-      }
-      document.getElementById("contributors-container-root").appendChild(typeContainer);
+xhr.addEventListener('load', function () {
+  var response = JSON.parse(xhr.responseText);
+  for (var type in response) {
+    var typeContent = response[type];
+    var typeContainer = document.createElement('div');
+    var typeHeader = document.createElement('h3');
+    typeHeader.innerText = type;
+    typeContainer.className = "contributors-type-container";
+    var avatarsContainer = document.createElement('div');
+    avatarsContainer.className = 'contributors-avatar-container';
+    typeContainer.appendChild(typeHeader);
+    typeContainer.appendChild(avatarsContainer);
+    for (var name in typeContent) {
+      var userContent = typeContent[name];
+      var avatar = document.createElement('div');
+      var img = document.createElement('img');
+      var sn = document.createElement('a');
+      avatar.setAttribute('class', 'wrap');
+      img.setAttribute('src', userContent.img);
+      sn.innerText = name;
+      sn.setAttribute('class', 'screen-name');
+      sn.setAttribute('href', "https://github.com/" + name);
+      sn.setAttribute('target', '_blank');
+      avatar.appendChild(img);
+      avatar.appendChild(sn);
+      avatarsContainer.appendChild(avatar);
     }
+    document.getElementById("contributors-container-root").appendChild(typeContainer);
+  }
 });
 var devtoolOpened = false;
-function openDevtool(){
-  if(!devtoolOpened){
+function openDevtool() {
+  if (!devtoolOpened) {
     var scTag = document.createElement("script");
     scTag.src = "https://unpkg.com/grimoirejs-inspector@1.0.19/dist/devtool.browser.js";
     document.body.appendChild(scTag);
@@ -42,3 +42,8 @@ function openDevtool(){
     devtoolOpened = true;
   }
 }
+
+gr(() => {
+  const w = document.body.clientWidth;
+  gr("#top")("goml").setAttribute("width", Math.min(800, w));
+});
